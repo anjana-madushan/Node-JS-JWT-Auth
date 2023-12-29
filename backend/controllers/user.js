cbcbd56ef618ff179e42f1362bbca83d3e2f0780
AnjanaPasinduMadushan <pbapmadushan@gmail.com>
2023-12-29 13:57:20 +0530

    Login function completed

--- backend/controllers/user.js
+++ backend/controllers/user.js
@@ -49,3 +49,45 @@ export const signUp = async (req, res) => {
 
 }
 
+export const login = async (req, res) => {
+
+  const { email, password } = req.body;
+
+  console.log(email, password)
+
+  //checking whether pasword and login fields are filled or not 
+  if (!email || !password) {
+    return res.status(422).json({ message: "All feilds should be filled" })
+  }
+
+  let loggedUser;
+
+  try {
+    loggedUser = await User.findOne({ email: email });
+
+    if (!loggedUser) {
+      return res.status(404).json({ message: "Email is not found, Check it and try again" })
+    }
+    //checking password and compare it with exist user's password in the db
+    const isPasswordCorrect = bcrypt.compareSync(password, loggedUser.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: "Invalid password, Check it and try again" })
+    }
+    const token = CreateToken(loggedUser._id)
+
+    //Create and setting a cookie with the user's ID and token
+    res.cookie(String(loggedUser._id), token, {
+      path: "/",
+      expires: new Date(Date.now() + 1000 * 60 * 20),
+      httpOnly: true,//if this option isn't here cookie will be visible to the frontend
+      sameSite: "lax"
+    })
+
+    //we send this msg along with the token and user details
+    return res.status(200).json({ message: "Successfully logged in", User: loggedUser, token })
+  } catch (err) {
+    console.log(err)
+  }
+
+
+}
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,8 @@
 import express from 'express';
 const router = express.Router();
-import { signUp } from "../controllers/user.js";
+import { signUp, login } from "../controllers/user.js";
 
 router.post("/signUp", signUp);
+router.post("/login", login);
 
 export default router;
